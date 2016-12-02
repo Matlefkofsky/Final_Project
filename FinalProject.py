56bf0b369e500d00fbfc60ee470ce063de74cb5e
Mathew Lefkofsky <MatLefkofsky@0587337728.wireless.umich.net>
2016-12-02 17:09:28 -0500

    added comments and fixed small things

--- FinalProject.py
+++ FinalProject.py
@@ -3,48 +3,25 @@ from random import randint
 import pygame
 import time
 import random
-
-"""A:  Create a variation of PacMan, Breakout, Asteroids.    You can create a game of your choice, but it must include movement, collision detection, scoring and/or time, and animation. All games should include sound. Base score is 100/150 pts for a working game.
-
-B: Show at least two consistent weeks of commits (at least 10 different days, more than 5 hours apart) 10pts
-
-C: Demonstrate a unique implementation of Class Inheritance 10pts
-
-D: Final 30 points are on a curved scale.  The class will rank the best games in the class."""
  
-class Trump_Food(pygame.sprite.Sprite):
-	x = 0
-	y = 0
-	multiplier = 47
+Blue = (0, 0, 255)
  
-	def __init__(self,x,y):
-		self.x = x * self.multiplier
-		self.y = y * self.multiplier
- 
-	def draw(self, surface, image):
-		surface.blit(image,(self.x, self.y)) 
- 
- 
-class Trump(pygame.sprite.Sprite):
+class Trump(pygame.sprite.Sprite): #class for snake 
 	x = [0]
 	y = [0]
-	multiplier = 47
+	Cell_Size = 45 #allows snake to seperate the board and stay in paths
 	direction = 0
-	global length
+	global length #globalize this to print the length later on when you die
 	length = 3
 	NewCountMax = 2
 	NewCount = 0
  
-	def __init__(self, length):
-	    self.length = length
-	    for x in range(0,5000):
-		    self.x.append(-100)
-		    self.y.append(-100)
- 
-	   # initial positions
-	    self.x[1] = 1*47
-	    self.x[2] = 2*47
- 
+	def __init__(self, length): #intializes the legnth of the snake and appends it to grow 
+		self.length = length
+		for z in range(0,1000): #set high to avoid issue of eating too much food and bugging out
+			self.x.append(-100)
+			self.y.append(-100)
+
 	def update(self):
  
 		self.NewCount = self.NewCount + 1
@@ -52,22 +29,22 @@ class Trump(pygame.sprite.Sprite):
  
 			# this is used to update previous positions
 			for x in range(self.length-1,0,-1):
-				self.x[x] = self.x[x-1]
+				self.x[x] = self.x[x-1] #x-1 makes it so it only takes 1 piece of food/imgage for trump to grow when he eats it
 				self.y[x] = self.y[x-1]
  
 			# these lines of code update the position of the head of the snake (the first Donald)
 			if self.direction == 0:
-				self.x[0] = self.x[0] + self.multiplier
+				self.x[0] = self.x[0] + self.Cell_Size
 			if self.direction == 1:
-				self.x[0] = self.x[0] - self.multiplier
+				self.x[0] = self.x[0] - self.Cell_Size
 			if self.direction == 2:
-				self.y[0] = self.y[0] - self.multiplier
+				self.y[0] = self.y[0] - self.Cell_Size
 			if self.direction == 3:
-				self.y[0] = self.y[0] + self.multiplier
+				self.y[0] = self.y[0] + self.Cell_Size
  
 			self.NewCount = 0
  
- 
+ #Defines the directions for the snake to go
 	def goR(self):
 		self.direction = 0
  
@@ -82,110 +59,119 @@ class Trump(pygame.sprite.Sprite):
  
 	def draw(self, surface, image):
 		for i in range(0,self.length):
-			surface.blit(image,(self.x[i],self.y[i])) 
+			surface.blit(image,(self.x[i],self.y[i])) #blits the surface for each part of the snake and continuosly updates
+
+
+class Trump_Food(pygame.sprite.Sprite):
+	x = 0
+	y = 0
+	Cell_Size = 45
+ 
+	def __init__(self,x,y): #intializes the snake food and size/placement of each piece
+		self.x = x * self.Cell_Size
+		self.y = y * self.Cell_Size
+ 
+	def draw(self, surface, image):
+		surface.blit(image,(self.x, self.y)) #draws the image on the surface
  
 class Game:
-	def Colliding(self,x1,y1,x2,y2):
+	def Colliding(self,x1,y1,x2,y2): #function recognizes when the snake is colliding with another object
 		if x1 >= x2 and x1 <= x2:
 			if y1 >= y2 and y1 <= y2:
 				return True
 		return False
- 
-class App(pygame.sprite.Sprite):
+
+class App(pygame.sprite.Sprite): #class acts as main function initializing the game
  
 	Width = 800
 	Height = 600
 	trump = 0
 	Trump_Food = 0
  
-	def __init__(self):
+	def __init__(self): #initilaizes all the methods of self 
 		self._running = True
 		self._display_surf = None
 		self._image_surf = None
 		self._Trump_Food_surf = None
 		self.game = Game()
-		self.trump = Trump(3) 
-		self.Trump_Food = Trump_Food(5,5)
+		self.trump = Trump(3) #original length of trump snake
+		self.Trump_Food = Trump_Food(1,1)
  
-	def Begin(self):
+	def Begin(self): 
 		pygame.init()
 		self._display_surf = pygame.display.set_mode((self.Width,self.Height), pygame.HWSURFACE)
- 
-		pygame.display.set_caption('Trump Game')
-		self._running = True
-		self._image_surf = pygame.image.load("trump.bmp").convert()
+		#initilaizes pygame and the display with the width and height
 
+		pygame.display.set_caption('Trump Game') #sets the caption
+		self._running = True
+		self._image_surf = pygame.image.load("Trump.bmp").convert() #loads the trump image for the snake
+
+		#this code creates a list of images that it will randomly select one of to be the snake food for trump to eat
 		lst_of_pics = ["hillary3.bmp","mexico.bmp"]
 		random_pic = random.choice(lst_of_pics)
 		self._Trump_Food_surf = pygame.image.load(random_pic).convert()
 
 		global sound
-		sound = pygame.mixer.Sound("Best_of_Donald.wav")
+		sound = pygame.mixer.Sound("Fired.wav") #imports soundtrack of Trump repeating "you're fired!"
  
-	def Event(self, event):
+	def Event(self, event): #function to quit out of game
 		if event.type == QUIT:
 			self._running = False
  
 	def Update(self):
 		self.trump.update()
  
-		# does snake eat Trump_Food?
+		# does snake eat Trump_Food? 
 		for i in range(0,self.trump.length):
 			if self.game.Colliding(self.Trump_Food.x,self.Trump_Food.y,self.trump.x[i], self.trump.y[i]):
-				self.Trump_Food.x = randint(2,9) * 47
-				self.Trump_Food.y = randint(2,9) * 47
-				self.trump.length = self.trump.length + 1
+				self.Trump_Food.x = randint(2,9) * 45
+				self.Trump_Food.y = randint(2,9) * 45 #If collision then another piece of food is placed
+				self.trump.length = self.trump.length + 1 #trump snake gains 1
 				# print (self.trump.length)
  
  
 		# does snake collide with itself?
 		for i in range(2,self.trump.length):
-			if self.game.Colliding(self.trump.x[0],self.trump.y[0],self.trump.x[i], self.trump.y[i]):
+			if self.game.Colliding(self.trump.x[0],self.trump.y[0],self.trump.x[i], self.trump.y[i]): #recognizes collision with self
 				print("Game over. You collided with yourself! Your score was ", (self.trump.length))
-				exit(0)
+				exit(0) #exits game and prints the length of the snake as the score of the game
  
 		pass
  
-	def Create(self):
+	def Create(self): #draws every display mentioned above 
 		self._display_surf.fill((0,0,0))
 		self.trump.draw(self._display_surf, self._image_surf)
 		self.Trump_Food.draw(self._display_surf, self._Trump_Food_surf)
-		pygame.display.flip()
- 
-	def quit(self):
-		pygame.quit()
+		pygame.display.flip() 
  
 	def running(self):
 		if self.Begin() == False:
 			self._running = False
 
 		while (self._running):
-			sound.play()
-			pygame.event.pump()
-			keys = pygame.key.get_pressed() 
+			sound.play() #plays the sound
 			for event in pygame.event.get():
-				if event.type == pygame.KEYDOWN:
+				if event.type == pygame.KEYDOWN: #when a key is pressed...
 					if event.key == pygame.K_LEFT:
-						self.trump.goL()
+						self.trump.goL() #if left arrow then go left
 					if event.key == pygame.K_RIGHT:
-						self.trump.goR()
+						self.trump.goR() #if right arrow then go right
 					if event.key == pygame.K_UP:
-						self.trump.goU()
+						self.trump.goU() #if up arrow then go up
 					if event.key == pygame.K_DOWN:
-						self.trump.goD()
+						self.trump.goD() #if down arrow then go down
 					if event.key == pygame.K_ESCAPE:
-						self._running = False
+						self._running = False #if escape button then quit program and print score
 						print ("You quit out of the game! Your score was ", self.trump.length)
 				if event.type == pygame.QUIT:
 					self._running = False
 					print ("You quit out of the game! Your score was ", self.trump.length)
- 
-			self.Update()
+						#if red x button then quit program and print score
+
+			self.Update() #calls update and create throughout the course of the game to make sure it runs smoothly
 			self.Create()
+		pygame.quit()
  
-			time.sleep (50.0 / 1000.0);
-		self.quit()
- 
-if __name__ == "__main__" :
+if __name__ == "__main__" : #run the game
 	theApp = App()
 	theApp.running()
